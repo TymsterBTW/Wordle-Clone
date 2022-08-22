5609bafe3aa87dd90ac09aae4d1f1527e955feb3
tymek lewandowski <tymek@tymeks-air.home>
2022-08-22 15:36:28 +0200

    Something

--- src/App.js
+++ src/App.js
@@ -1,63 +1,34 @@
-import {  useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Grid from "./Grid";
+import Title from "./Title";
 import Words from "./Words";
 function App() {
-  const [position , setposition] = useState([0,0])  // first refers to row and second refers to place'
-  const [save , setsave] = useState([])
-  const [currentguess , setcurrentguess] = useState(["","","","",""])
-  const [output , setoutput ] = useState([])
-  var word = ""
-  useEffect(() => {
-    let load = []
-    for(let i = 0; i < 30; i ++){
-      load.push("")
-    }
-    setoutput(load)
-    word = Words[Math.floor(Math.random() * Words.length - 1)]
-    console.log(word)
-  },[])
-  
-  function updateoutput(){
-    console.log("inside function")
-    let newoutput = output
-    newoutput[(position[0] * 5) + position[1]] = currentguess[position[1]]
-    console.log(newoutput)
-    setoutput(newoutput)
-  }
-  function CheckWord(){
-    
-  }
-  document.onkeypress = function (e) {
-    console.log(typeof currentguess)
-    let c = e.keyCode
-    if(currentguess.length > 5 ){
-      if(c == 8){
-        setcurrentguess(currentguess.pop())
-        updateoutput()
-      }else if(c==13){
-        console.log("enter")
-        CheckWord()
+  const [row , setrow] = useState(0)
+  const [output , setoutput] = useState(() => {
+    let temp = [[],[],[],[],[],[]]
+    temp.forEach(item => {
+      for(let i = 0; i < 5; i ++){
+        item.push("Test")
       }
-    }else if (96 < c && c < 123 || 64 < c && c < 91){
-      console.log(e.key.toUpperCase())
-      console.log(typeof currentguess)
-      let temp = currentguess.push(e.key.toUpperCase())
-      setcurrentguess(temp)
-      updateoutput()
-      setposition([position[0] , position[1] + 1])
-    }else if(c == 8){
-      console.log("pop")
-      setcurrentguess(currentguess.pop())
-      updateoutput()
+    })
+    console.log(temp)
+    return temp;
+
+  })
+  //let temp = [..output] then modify and setoutput(temp)
+  document.onkeypress = function(e){
+
+    let c = e.keyCode
+    if((64 < c && c < 91) || (96 < c && c < 123)){
+      console.log(e.key)
     }
   }
   return (
-    <div className="App" onKeyPress={e => {console.log("pressed")}}>
-      <div className="title-container">
-        <h1 className="title">Wordle Clone</h1>
-      </div>
+    <div className="App">
+      <Title/>
       <div className="input-container">
-        {output.map(value => (
-          <div className=" InputField" key={Math.random()}>{value}</div>
+        {output.map(row => (
+          <Grid key={Math.random()} list={row}/>
         ))}
       </div>
     </div>
