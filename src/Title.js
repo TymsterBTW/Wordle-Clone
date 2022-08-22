37dafafd73d5a3161c5da65ca6ebe8e085836b32
tymek lewandowski <tymek@tymeks-air.home>
2022-08-22 15:37:49 +0200

    S

---
+++ src/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <div className="title-container">
+            <h1 className="title">Worlde Infinite</h1>
+        </div>
+    );
+}
+ 
+export default Title;
